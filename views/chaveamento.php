426ed169de8696a24c212a9966f3ed1dc13319ad
Pedro-CMLDU <pedrolsilva@petrolina.pe.senac.br>
2024-11-08 09:30:47 -0500

    Alteraçao no controller x

--- views/chaveamento.php
+++ views/chaveamento.php
@@ -9,16 +9,15 @@
 <body>
     <div class="retangulo-superior">
         <div class="keybox">
-         <img src="../imagem/logo.png" alt="keybox">
+            <img src="../imagem/logo.png" alt="keybox">
         </div>
         <div class="imglogosenac">
-         <img src="../imagem/logosenac.png" alt="logosenac" class="img_senac_logo">
-     </div>
-     </div>
+            <img src="../imagem/logosenac.png" alt="logosenac" class="img_senac_logo">
+        </div>
     </div>
     
     <nav class="navegacao">
-        <a href="index_menu.php">Início</a> >
+        <a href="index_menu.php">Início</a> &gt;
         <a href="fucionario.php">Voltar</a>
     </nav>
 
@@ -28,13 +27,37 @@
             <input type="search" name="" id="" placeholder="Pesquisa" class="pesquisa">
         </div>        
     </section>
+
     <section class="blocoprincipal">
         <div class="input-chave">
             <img src="../imagem/lupa.png" alt="lupa">
-            <button type="submit" name="" id="" class="chave"> Chave (0) </button>
+            <button type="submit" name="" id="" class="chave"> Chave (<?php echo count($chaves); ?>) </button>
             <img src="../imagem/interrogacao.png" alt="interrogação" class="img-interrogação">
-        </div> 
-        <img src="../imagem/keys 2.png" alt="chave vazias" class="chave-vazia">
+        </div>
+
+        <!-- Lista de Chaves -->
+        <?php if (count($chaves) > 0): ?>
+            <table class="tabela-chaves">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Descrição</th>
+                        <th>Número</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($chave as $chave): ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($chave['id']); ?></td>
+                            <td><?php echo htmlspecialchars($chave['descricao']); ?></td>
+                            <td><?php echo htmlspecialchars($chave['numero']); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else: ?>
+            <p class="mensagem-vazia">Nenhuma chave cadastrada.</p>
+        <?php endif; ?>
     </section>
 </body>
 </html>
